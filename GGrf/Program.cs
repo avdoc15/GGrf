0de0630cde403e870257f41727c824358398a8ec
исп-43 <-43@LAB30-11>
2021-11-30 08:31:55 +0300

    Добавьте файлы проекта.

---
+++ GGrf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GGrf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
